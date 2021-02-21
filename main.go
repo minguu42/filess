388dbbea8ce33b940de1a8503acdb5404555da53
minguu <minguu42@gmail.com>
2021-02-21 17:10:21 +0900

    シングルコマンドパターンのCLIの作り方を学習した

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Start of main")
+	envPort, _ := strconv.Atoi(os.Getenv("PORT"))
+	var port int
+	flag.IntVar(&port, "port", envPort, "port to use")
+	flag.IntVar(&port, "p", envPort, "port to use (short)")
+
+	flag.Parse()
+	fmt.Println(port)
+	fmt.Println("End of main")
+}
