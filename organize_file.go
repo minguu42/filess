8f16099190062416b6ad5b6e2f3f43f0071f977c
minguu <minguu42@gmail.com>
2021-02-24 10:50:05 +0900

    概要を理解するところまで

--- organize_file.go
+++ organize_file.go
@@ -1,5 +1,29 @@
 package filess
 
-func OrganizeFile(config Config) {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
 
+func OrganizeFile(config Config) {
+	// ターゲットディレクトリとソースディレクトリの取得
+	targets := config.Targets
+	sources := config.Sources
+
+	// 分類するためのプリフィックスの取得
+
+
+	// ソースディレクトリを一つずつ調べる
+	for _, source := range sources {
+		files, err := ioutil.ReadDir(source)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			fmt.Println(filepath.Join(source, file.Name()))
+		}
+	}
+	fmt.Println(targets)
 }
