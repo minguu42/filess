20e1a68a704b8a926b524f31635770580280dd09
minguu <minguu42@gmail.com>
2021-02-25 13:03:32 +0900

    コマンドでバージョンを表示する

--- cmd/filess/main.go
+++ cmd/filess/main.go
@@ -6,10 +6,19 @@ import (
 	"github.com/minguu42/filess"
 )
 
-func main() {
-	flag.Parse()
-	arg := flag.Arg(0)
+var version = "0.1.0"
 
+func main() {
+	var showVersion bool
+	flag.BoolVar(&showVersion, "v", false, "show version")
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.Parse()
+	if showVersion {
+		fmt.Println("version:", version)
+		return
+	}
+
+	arg := flag.Arg(0)
 	if arg == "" {
 		fmt.Println("JSONファイルが指定されていません。")
 	} else {
